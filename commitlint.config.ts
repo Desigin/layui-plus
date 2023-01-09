3253634ada5df8502d17722d806368151236559d
ch930410 <84368821@qq.com>
2023-01-09 14:44:40 +0800

    feat: 增加theme主题

---
+++ commitlint.config.ts
@@ -0,0 +1,11 @@
+// 'feat', // 新功能
+// 'fix', //  修复
+// 'docs', // 文档变更
+// 'style', // 代码格式（不影响代码运行的变动）
+// 'refactor', // 重构（既不是增加feature）,也不是修复bug
+// 'pref', // 性能优化
+// 'test', // 增加测试
+// 'chore', // 构建过程或辅助工具的变动
+// 'revert', // 回退
+// 'build' // 打包
+const types = ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'release', 'chore', 'revert'];
